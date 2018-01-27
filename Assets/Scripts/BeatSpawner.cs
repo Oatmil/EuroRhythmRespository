eaa37c19ddddcb84581f60701a3058d71efc14a9
oatmil <limtaojuen@gmail.com>
2018-01-28 03:56:42 +0800

    New Timing and Gear stick Implementation
    Things left hand and Clean up and fix up beat maps

--- Assets/Scripts/BeatSpawner.cs
+++ Assets/Scripts/BeatSpawner.cs
@@ -9,7 +9,7 @@ public class BeatSpawner : MonoBehaviour {
     public GameObject EndPoint;
     public GameObject StartPoint;
 
-    public GameObject BAR;
+    public List<GameObject> BAR_Object;
     public List<GameObject> BAR_List;
 
     [Header("The List for the bars on queue")]
@@ -22,32 +22,39 @@ public class BeatSpawner : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < 3; i++)
         {
-			GameObject tempObj = GameObject.Instantiate(BAR, transform);
-            tempObj.SetActive(false);
-            BAR_List.Add(tempObj);
+            foreach (GameObject tempBar in BAR_Object)
+            {
+                GameObject tempObj = GameObject.Instantiate(tempBar, transform);
+                tempObj.SetActive(false);
+                BAR_List.Add(tempObj);
+            }
         }
-	}
+        
+    }
 	
 	// Update is called once per frame
 	void Update () {
 
 	}
 
-    public void SpawnNote()
+    public void SpawnNote(int i_point) // start from 1 - 9 from top left and across and than down
     {
-        foreach(GameObject obj in BAR_List)
+        for (int i = i_point - 1; i < BAR_List.Capacity ; i+= BAR_Object.Count)
         {
-            if(obj.activeSelf == false)
+            if (BAR_List[i].activeSelf == false)
             {
-                obj.transform.position = StartPoint.transform.position;
-                obj.GetComponent<BeatScript>().f_speed = Vector3.Distance(EndPoint.transform.position, StartPoint.transform.position) / SCORE_Manager.m_instance.f_NoteSpeed;
-                obj.GetComponent<BeatScript>().endPoint = EndPoint.transform.position;
-                Queue_List.Add(obj);
-                obj.SetActive(true);
+                BAR_List[i].transform.position = StartPoint.transform.position;
+                BAR_List[i].transform.GetChild(0).transform.localScale = StartPoint.transform.localScale;
+
+                BAR_List[i].GetComponent<BeatScript>().f_speed = Vector3.Distance(EndPoint.transform.localScale, StartPoint.transform.localScale) / SCORE_Manager.m_instance.f_NoteSpeed;
+                BAR_List[i].GetComponent<BeatScript>().endPoint = EndPoint;
+                Queue_List.Add(BAR_List[i]);
+                BAR_List[i].SetActive(true);
                 break;
             }
         }
+        
     }
 }
--- Assets/Scripts/XmlManager.cs
+++ Assets/Scripts/XmlManager.cs
@@ -7,238 +7,261 @@ using UnityEngine;
 
 public class XmlManager : MonoBehaviour
 {
+    [Header("")]
+    public float A_AudioDelay;
 
-	public float A_AudioDelay;
+    bool XmlLoaded = false;
+    XmlDocument XMLDoc;
 
-	bool XmlLoaded = false;
-	XmlDocument XMLDoc;
+    [Header("the path or name of the particular XML")]
+    public string[] xmlpath;
 
-	[Header ("the path or name of the particular XML")]
-	public string[] xmlpath;
+    bool b_Respond = false;
 
-	bool b_Respond = false;
+    public bool SpaceBar = false;
+    bool UpArrow = false;
+    bool DownArrow = false;
+    bool LeftArrow = false;
+    bool RightArrow = false;
 
-	public bool SpaceBar = false;
-	bool UpArrow = false;
-	bool DownArrow = false;
-	bool LeftArrow = false;
-	bool RightArrow = false;
-    
-	public GameObject testManualStick;
-    
-	// Use this for initialization
-	void Start ()
-	{
-		LoadXML (2);
-		StartCoroutine (PRINTXML_NOTES (2));
-		Debug.Log ("finish");
-	}
+    public GameObject testManualStick;
 
-	private void Update ()
-	{
-		if (b_Respond) {
-			if (Input.GetKeyDown (KeyCode.Space))
-				SpaceBar = true;
-			if (Input.GetKeyDown (KeyCode.UpArrow))
-				UpArrow = true;
-			if (Input.GetKeyDown (KeyCode.DownArrow))
-				DownArrow = true;
-			if (Input.GetKeyDown (KeyCode.LeftArrow))
-				LeftArrow = true;
-			if (Input.GetKeyDown (KeyCode.RightArrow))
-				RightArrow = true;
-		}
-	}
+    // Use this for initialization
+    void Start()
+    {
+        LoadXML(0);
+        StartCoroutine(PRINTXML_NOTES(0));
+        Debug.Log("finish");
+    }
 
-	IEnumerator WRITEXML (int i_level)
-	{
-		b_Respond = true;
-		bool b_stop = false;
-		float f_Timer = 0;
-		AudioManager.m_instace.Select_Song (i_level);
-		yield return new WaitUntil (() => (SpaceBar == true));
+    private void Update()
+    {
+        if (b_Respond)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+                SpaceBar = true;
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+                UpArrow = true;
+            if (Input.GetKeyDown(KeyCode.DownArrow))
+                DownArrow = true;
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+                LeftArrow = true;
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+                RightArrow = true;
+        }
+    }
 
-		yield return new WaitForEndOfFrame ();
-		SpaceBar = false;
+    IEnumerator WRITEXML(int i_level)
+    {
+        b_Respond = true;
+        bool b_stop = false;
+        float f_Timer = 0;
+        AudioManager.m_instace.Select_Song(i_level);
+        yield return new WaitUntil(() => (SpaceBar == true));
 
-		Debug.Log ("Start");
-		AudioManager.m_instace.PlaySong ();
-		b_stop = false;
+        yield return new WaitForEndOfFrame();
+        SpaceBar = false;
 
-		yield return new WaitForEndOfFrame ();
-		do {
-			f_Timer += Time.deltaTime;
-			if (UpArrow) {
-				testManualStick.transform.position += new Vector3 (0, 1, 0);
-				UpArrow = false;
-				AddNewNote ("UP", f_Timer.ToString ());
-			}
-			if (DownArrow) {
-				testManualStick.transform.position += new Vector3 (0, -1, 0);
-				DownArrow = false;
-				AddNewNote ("DOWN", f_Timer.ToString ());
-			}
-			if (LeftArrow) {
-				testManualStick.transform.position += new Vector3 (-1, 0, 0);
-				LeftArrow = false;
-				AddNewNote ("LEFT", f_Timer.ToString ());
-			}
-			if (RightArrow) {
-				testManualStick.transform.position += new Vector3 (1, 0, 0);
-				RightArrow = false; 
-				AddNewNote ("RIGHT", f_Timer.ToString ());
-			}
+        Debug.Log("Start");
+        AudioManager.m_instace.PlaySong();
+        b_stop = false;
 
-			if (SpaceBar) {
-				SpaceBar = false;
-				b_stop = true;
-			}
-			yield return new WaitForEndOfFrame ();
-		} while (!b_stop);
+        yield return new WaitForEndOfFrame();
+        do
+        {
+            f_Timer += Time.deltaTime;
+            if (UpArrow)
+            {
+                testManualStick.transform.position += new Vector3(0, 1, 0);
+                UpArrow = false;
+                AddNewNote("UP", f_Timer.ToString());
+            }
+            if (DownArrow)
+            {
+                testManualStick.transform.position += new Vector3(0, -1, 0);
+                DownArrow = false;
+                AddNewNote("DOWN", f_Timer.ToString());
+            }
+            if (LeftArrow)
+            {
+                testManualStick.transform.position += new Vector3(-1, 0, 0);
+                LeftArrow = false;
+                AddNewNote("LEFT", f_Timer.ToString());
+            }
+            if (RightArrow)
+            {
+                testManualStick.transform.position += new Vector3(1, 0, 0);
+                RightArrow = false;
+                AddNewNote("RIGHT", f_Timer.ToString());
+            }
 
-		Debug.Log ("Save");
-		SaveXML (i_level);
-		yield return null;
-	}
+            if (SpaceBar)
+            {
+                SpaceBar = false;
+                b_stop = true;
+            }
+            yield return new WaitForEndOfFrame();
+        } while (!b_stop);
 
-	IEnumerator SpawnNotes (XmlNodeList nodeList)
-	{
-		bool b_stop = false;
-		float f_Timer = 0 + SCORE_Manager.m_instance.f_NoteSpeed + A_AudioDelay;
-		int i = 0;
-		do {
-			f_Timer += Time.deltaTime;
-			if (nodeList [i] != null) {
-				if (float.Parse (nodeList [i].SelectSingleNode ("time").InnerText) <= f_Timer) {
-					// Debug.Log(nodeList[i].SelectSingleNode("Direction").InnerText);
-					switch (nodeList [i].SelectSingleNode ("Direction").InnerText) {
-					case "UP":
-						BeatSpawner.m_instance.SpawnNote ();
-						break;
-					case "DOWN":
-						BeatSpawner.m_instance.SpawnNote ();
-						break;
-					case "RIGHT":
-						BeatSpawner.m_instance.SpawnNote ();
-						break;
-					case "LEFT":
-						BeatSpawner.m_instance.SpawnNote ();
-						break;
-					}
+        Debug.Log("Save");
+        SaveXML(i_level);
+        yield return null;
+    }
 
-					i++;
-				}
-			}
-			yield return new WaitForEndOfFrame ();
-		} while (!b_stop);
-		yield return null;
-	}
+    IEnumerator SpawnNotes(XmlNodeList nodeList)
+    {
+        bool b_stop = false;
+        float f_Timer = 0 + SCORE_Manager.m_instance.f_NoteSpeed + A_AudioDelay;
+        int i = 0;
+        do
+        {
+            f_Timer += Time.deltaTime;
+            if (nodeList[i] != null)
+            {
+                if (float.Parse(nodeList[i].SelectSingleNode("time").InnerText) <= f_Timer)
+                {
+                    // Debug.Log(nodeList[i].SelectSingleNode("Direction").InnerText);
+                    switch (nodeList[i].SelectSingleNode("Direction").InnerText)
+                    {
+                        case "UP":
+                            BeatSpawner.m_instance.SpawnNote(int.Parse(nodeList[i].SelectSingleNode("stick").InnerText));
+                            break;
+                        case "DOWN":
+                            BeatSpawner.m_instance.SpawnNote(int.Parse(nodeList[i].SelectSingleNode("stick").InnerText));
+                            break;
+                        case "RIGHT":
+                            BeatSpawner.m_instance.SpawnNote(int.Parse(nodeList[i].SelectSingleNode("stick").InnerText));
+                            break;
+                        case "LEFT":
+                            BeatSpawner.m_instance.SpawnNote(int.Parse(nodeList[i].SelectSingleNode("stick").InnerText));
+                            break;
+                    }
 
-	IEnumerator PRINTXML_NOTES (int i_level)
-	{
-		b_Respond = true;
-		bool b_stop = false;
-		float f_Timer = 0;
-		AudioManager.m_instace.Select_Song (i_level);
-		yield return new WaitUntil (() => (SpaceBar == true));
-		SpaceBar = false;
+                    i++;
+                }
+            }
+            yield return new WaitForEndOfFrame();
+        } while (!b_stop);
+        yield return null;
+    }
 
-		XmlNodeList nodeList = XMLDoc.SelectSingleNode ("XML").ChildNodes;
-		yield return new WaitForEndOfFrame ();
-		Debug.Log ("Start");
-		AudioManager.m_instace.PlaySong ();
-		b_stop = false;
-		int i = 0;
-		float f_Scaller = 0;
-		yield return new WaitForEndOfFrame ();
-		StartCoroutine (SpawnNotes (nodeList));
-		do {
-			f_Timer += Time.deltaTime;
-			if (nodeList [i] != null) {
-				if (float.Parse (nodeList [i].SelectSingleNode ("time").InnerText) <= f_Timer) {
-					// Debug.Log(nodeList[i].SelectSingleNode("Direction").InnerText);
-					switch (nodeList [i].SelectSingleNode ("Direction").InnerText) {
-					case "UP":
-						testManualStick.transform.position += new Vector3 (0, 1, 0);
-						break;
-					case "DOWN":
-						testManualStick.transform.position += new Vector3 (0, -1, 0);
-						break;
-					case "RIGHT":
-						testManualStick.transform.position += new Vector3 (1, 0, 0);
-						break;
-					case "LEFT":
-						testManualStick.transform.position += new Vector3 (-1, 0, 0);
-						break;
-					}
+    IEnumerator PRINTXML_NOTES(int i_level)
+    {
+        b_Respond = true;
+        bool b_stop = false;
+        float f_Timer = 0;
+        AudioManager.m_instace.Select_Song(i_level);
+        yield return new WaitUntil(() => (SpaceBar == true));
+        SpaceBar = false;
 
-					i++;
-				}
-			}
-			yield return new WaitForEndOfFrame ();
-		} while (!b_stop);
+        XmlNodeList nodeList = XMLDoc.SelectSingleNode("XML").ChildNodes;
+        yield return new WaitForEndOfFrame();
+        Debug.Log("Start");
+        AudioManager.m_instace.PlaySong();
+        b_stop = false;
+        int i = 0;
+        yield return new WaitForEndOfFrame();
+        StartCoroutine(SpawnNotes(nodeList));
+        do
+        {
+            f_Timer += Time.deltaTime;
+            if (nodeList[i] != null)
+            {
+                if (float.Parse(nodeList[i].SelectSingleNode("time").InnerText) <= f_Timer)
+                {
+                    // Debug.Log(nodeList[i].SelectSingleNode("Direction").InnerText);
+                    switch (nodeList[i].SelectSingleNode("Direction").InnerText)
+                    {
+                        case "UP":
+                            testManualStick.transform.position += new Vector3(0, 1.7f, 0);
+                            break;
+                        case "DOWN":
+                            testManualStick.transform.position += new Vector3(0, -1.7f, 0);
+                            break;
+                        case "RIGHT":
+                            testManualStick.transform.position += new Vector3(1.3f, 0, 0);
+                            break;
+                        case "LEFT":
+                            testManualStick.transform.position += new Vector3(-1.3f, 0, 0);
+                            break;
+                    }
 
-		yield return null;
-	}
+                    i++;
+                }
+            }
+            yield return new WaitForEndOfFrame();
+        } while (!b_stop);
 
-	IEnumerator DelayTest ()
-	{
-		Debug.Log ("Start");
-		bool bbbb = true;
-		bool baba = true;
-		float temptemp = 0;
-		do {
-			yield return new WaitUntil (() => (SpaceBar == true));
-			Debug.Log ("Time");
-			yield return new WaitForEndOfFrame ();
-			SpaceBar = false;
-			BeatSpawner.m_instance.SpawnNote ();
-			temptemp = 0;
-			do {
-				temptemp += Time.deltaTime;
-                
-				if (SpaceBar) {
-					SpaceBar = false;
-					baba = false;
-				}
-				yield return new WaitForEndOfFrame ();
-			} while (baba);
+        yield return null;
+    }
 
-			Debug.Log (temptemp);
-            
-		} while (bbbb);
-	}
+    IEnumerator DelayTest()
+    {
+        Debug.Log("Start");
+        bool bbbb = true;
+        bool baba = true;
+        float temptemp = 0;
+        do
+        {
+            yield return new WaitUntil(() => (SpaceBar == true));
+            Debug.Log("Time");
+            yield return new WaitForEndOfFrame();
+            SpaceBar = false;
+            baba = true;
+            BeatSpawner.m_instance.SpawnNote(5);
+            temptemp = 0;
+            do
+            {
+                temptemp += Time.deltaTime;
 
-	void LoadXML (int i) //Load the XML out to be used and access
-	{
-		if (!XmlLoaded) {
-			XMLDoc = new XmlDocument (); // create new xml document;
-			XMLDoc.Load (Path.Combine (Application.dataPath, xmlpath [i])); // replace online file with local file
-			XmlLoaded = true;
-		}
-	}
+                if (SpaceBar)
+                {
+                    SpaceBar = false;
+                    baba = false;
+                }
+                if (BeatSpawner.m_instance.Queue_List[0].transform.localScale.x <= 1)
+                {
+                    SpaceBar = false;
+                    baba = false;
+                }
+                yield return new WaitForEndOfFrame();
+            } while (baba);
 
-	void SaveXML (int i) // save any changes done to it.
-	{
-		XMLDoc.Save (Path.Combine (Application.dataPath, xmlpath [i]));
-	}
+            Debug.Log(temptemp);
 
-	void AddNewNote (string s_Direction, string s_time) // Add node under the main node
-	{
-		XmlElement notes = XMLDoc.CreateElement ("note");
+        } while (bbbb);
+    }
 
-		XmlElement time = XMLDoc.CreateElement ("time");
-		XmlElement DIRECTION = XMLDoc.CreateElement ("Direction");
+    void LoadXML(int i) //Load the XML out to be used and access
+    {
+        if (!XmlLoaded)
+        {
+            XMLDoc = new XmlDocument(); // create new xml document;
+            XMLDoc.Load(Path.Combine(Application.dataPath, xmlpath[i])); // replace online file with local file
+            XmlLoaded = true;
+        }
+    }
+    void SaveXML(int i) // save any changes done to it.
+    {
+        XMLDoc.Save(Path.Combine(Application.dataPath, xmlpath[i]));
+    }
 
-		time.InnerText = s_time;
-		DIRECTION.InnerText = s_Direction;
+    void AddNewNote(string s_Direction, string s_time) // Add node under the main node
+    {
+        XmlElement notes = XMLDoc.CreateElement("note");
 
-		notes.AppendChild (time);
-		notes.AppendChild (DIRECTION);
+        XmlElement time = XMLDoc.CreateElement("time");
+        XmlElement DIRECTION = XMLDoc.CreateElement("Direction");
 
-		XMLDoc.DocumentElement.AppendChild (notes);
-        
-	}
-    
-    
+        time.InnerText = s_time;
+        DIRECTION.InnerText = s_Direction;
+
+        notes.AppendChild(time);
+        notes.AppendChild(DIRECTION);
+
+        XMLDoc.DocumentElement.AppendChild(notes);
+
+    }
+
+
 }
